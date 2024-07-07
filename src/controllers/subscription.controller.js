844956c4b64d0f93e59c3aa6d59c429817bc9f06
Sayan Das <sayandasraja56@gmail.com>
2024-07-08 01:57:27 +0530

    Add toggle Subscription and channels Subscribed

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -46,6 +46,7 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
         //project the required fields
         {
             $project : {
+                _id: 0,
                 username : "$subscriberFromSubscriptionsModel.username",
                 email : "$subscriberFromSubscriptionsModel.email",
                 fullName : "$subscriberFromSubscriptionsModel.fullName"
@@ -54,8 +55,8 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
         }
     ])
 
-    //console.log(subscribers)
-    //console.log("Querying for channel ID:", channelId);
+    console.log(subscribers)
+    console.log("Querying for channel ID:", channelId);
 
     
 
@@ -71,8 +72,120 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
 })
 
 // controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    //STEPS :-
+    //1. collect subscriberId from req.params
+    //2. check if subscriberId is valid and exists
+    //3. find all channels to which user has subscribed
+    //4. return success response with channel list
 
+    //1. collect subscriberId from req.params
+    const { subscriberId } = req.params
+
+    //2. check if subscriberId is valid and exists
+    if (!isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Invalid Subscriber Id")
+    }
+
+    //3. find all channels to which user has subscribed
+    const channels = await Subscription.aggregate([
+        //match the subscriber id
+        {
+            $match: {
+                subscriber: new mongoose.Types.ObjectId(subscriberId)
+            }
+        },
+        //lookup the user collection to get the user details
+        {
+            $lookup: {
+                from: "users",
+                localField: "channel",
+                foreignField: "_id",
+                as: "subscriberSubscribedToChannel"
+            }
+        },
+        //unwind the subscriberSubscribedToChannel array to get the user details
+        {
+            $unwind: "$subscriberSubscribedToChannel"
+        },
+        //project the required fields
+        {
+            $project: {
+                _id: 0,
+                username: "$subscriberSubscribedToChannel.username",
+                email: "$subscriberSubscribedToChannel.email",
+                fullName: "$subscriberSubscribedToChannel.fullName"
+
+            }
+        }
+    ])
+
+    console.log(channels)
+    console.log("Querying for subscriber ID:", subscriberId);
+
+    //4. return success response with channel list
+    if (channels.length === 0) {
+        return res.status(200).json(
+            new ApiResponse(200, channels[0], "No channels found to which user has subscribed")
+        );
+    }
+    res.status(200).json(
+        new ApiResponse(214, channels[0], "Channels List")
+    )
+})
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+    //STEPS :-
+    //1. collect channelId from req.params
+    //2. check if channelId is valid and exists
+    //3. check if user is already subscribed to the channel
+    //4. if user is already subscribed then unsubscribe else subscribe
+    //5. return success response with channel list
+
+    //1. collect channelId from req.params
+    const {channelId} = req.params
+
+    //2. check if channelId is valid and exists
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400,"Invalid Channel Id")
+    }
+
+    //3. check if user is already subscribed to the channel
+    const user = req.user 
+    if(!user){
+        throw new ApiError(400,"user not logged in")
+    }
+
+    const subscriberId = user?._id
+
+    const subscription = await Subscription.findOne({
+        channel : channelId,
+        subscriber : subscriberId
+    })
+
+    //4. if user is already subscribed then unsubscribe else subscribe
+
+    if(subscription){
+        //unsubscribe
+        await Subscription.findByIdAndDelete(subscription?._id)
+        return res.status(200).json(
+            new ApiResponse(200,{},"Unsubscribed successfully")
+        )
+    }
+
+    //subscribe
+    await Subscription.create({
+        channel : channelId,
+        subscriber : subscriberId
+    })
+    res.status(200).json(
+        new ApiResponse(214,{},"Subscribed successfully")
+    )
+
+})
 
 export {
-    getUserChannelSubscribers
+    getUserChannelSubscribers,
+    getSubscribedChannels,
+    toggleSubscription
 }
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,13 +1,19 @@
 import { Router } from 'express';
 import {
     
-    getUserChannelSubscribers
+    getSubscribedChannels,
+    getUserChannelSubscribers,
+    toggleSubscription
 } from "../controllers/subscription.controller.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+//router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/u/:channelId").get(getUserChannelSubscribers);
+router.route("/c/:channelId").get(verifyJWT,getUserChannelSubscribers);
+
+router.route("/u/:subscriberId").get(verifyJWT, getSubscribedChannels);
+
+router.route("/c/:channelId").post(verifyJWT, toggleSubscription)
 
 export default router
